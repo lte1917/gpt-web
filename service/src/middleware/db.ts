e1f57e0c04a3d8a8826095817cc945e4df73e9af
LTE1916 <2252351619@qq.com>
2024-07-15 10:17:02 +0800

    add auto connect to mysql

--- service/src/middleware/db.ts
+++ service/src/middleware/db.ts
@@ -11,4 +11,23 @@ const pool = mysql.createPool({
 	reconnect: true
 });
 
+setInterval(() => {
+//check connection
+	pool.getConnection((err, connection) => {
+		if (err) {
+			if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+				console.error('Database connection was closed.');
+			}
+			if (err.code === 'ER_CON_COUNT_ERROR') {
+				console.error('Database has too many connections.');
+			}
+			if (err.code === 'ECONNREFUSED') {
+				console.error('Database connection was refused.');
+			}
+		}
+		if (connection) connection.release();
+		return;
+	});
+}, 1000*60*60);
+
 export { pool };
